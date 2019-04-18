581e8628f213cafc674e55765a30befd04494f98
tista500 <tista.gma500@gmail.com>
2019-04-18 23:07:02 +0900

    Extra: [Air-for-steam] Introduce color-changer mechanism (4)
    Styles.in file does not have any default (preset) primary/secondary colors:
     * No need to compare key-colors.scss with colors.scss.
     * Copy and embed RGB values from generated CSS directly.

--- src/extra/airforsteam/recolor-air.sh
+++ src/extra/airforsteam/recolor-air.sh
@@ -11,14 +11,7 @@
 #
 
 SRC_FILE="Resource/colors/plata-noir.styles"
-COL_FILE="../../gtk/sass/common/_colors.scss"
 KEY_FILE="../../gtk/sass/common/resources/_key_colors.scss"
-
-# Default colors
-selection1="`grep 'Indigo500' $COL_FILE | \
-                   cut -d' ' -f3`"
-accent1="`grep 'Indigo300' $COL_FILE | \
-                cut -d' ' -f3`"
 
 # Generate CSS for color source
 SASSC="`command -v sassc`"
@@ -26,51 +19,32 @@ SASSC_OPTION="-M -t nested"
 
 $SASSC $SASSC_OPTION sass/air-dark.scss sass/air-dark.css
 
-# Default RGB values
-RB1="63"
-GB1="81"
-BB1="181"
-RL1="158"
-GL1="169"
-BL1="228"
-
 # Pick up RGB values from CSS
-RB2="`grep -e rb sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
-GB2="`grep -e gb sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
-BB2="`grep -e bb sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
-RL2="`grep -e rl sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
-GL2="`grep -e gl sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
-BL2="`grep -e bl sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
+RB="`grep -e rb sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
+GB="`grep -e gb sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
+BB="`grep -e bb sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
+RL="`grep -e rl sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
+GL="`grep -e gl sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
+BL="`grep -e bl sass/air-dark.css | cut -d':' -f2 | cut -d';' -f1`"
 
 # Check and re-color color-scheme
 if [ -e $KEY_FILE ]; then
-    selection2="`grep 'key_selection' $KEY_FILE | \
-                 cut -d' ' -f2 | cut -d';' -f1`"
-    accent2="`grep 'key_accent' $KEY_FILE | \
-              cut -d' ' -f2 | cut -d';' -f1`"
+    selection="`grep 'key_selection' $KEY_FILE | \
+                cut -d' ' -f2 | cut -d';' -f1`"
+    accent="`grep 'key_accent' $KEY_FILE | \
+             cut -d' ' -f2 | cut -d';' -f1`"
 
     cp -f $SRC_FILE.in $SRC_FILE && sleep 1
 
-    if [ $selection1 = $selection2 ]; then
-        sed -i "s/@R_BG@/$RB1/gi" $SRC_FILE
-        sed -i "s/@G_BG@/$GB1/gi" $SRC_FILE
-        sed -i "s/@B_BG@/$BB1/gi" $SRC_FILE
-    else
-        sed -i "s/@R_BG@/$RB2/gi" $SRC_FILE
-        sed -i "s/@G_BG@/$GB2/gi" $SRC_FILE
-        sed -i "s/@B_BG@/$BB2/gi" $SRC_FILE
-        echo re-colored with $selection2.
-    fi
+    echo Setting selection-color $selection...
+    sed -i "s/@R_BG@/$RB/gi" $SRC_FILE
+    sed -i "s/@G_BG@/$GB/gi" $SRC_FILE
+    sed -i "s/@B_BG@/$BB/gi" $SRC_FILE
 
-    if [ $accent1 = $accent2 ]; then
-        sed -i "s/@R_LABEL@/$RL1/gi" $SRC_FILE
-        sed -i "s/@G_LABEL@/$GL1/gi" $SRC_FILE
-        sed -i "s/@B_LABEL@/$BL1/gi" $SRC_FILE
-    else
-        sed -i "s/@R_LABEL@/$RL2/gi" $SRC_FILE
-        sed -i "s/@G_LABEL@/$GL2/gi" $SRC_FILE
-        sed -i "s/@B_LABEL@/$BL2/gi" $SRC_FILE
-    fi
+    echo Setting accent-label-color $accent...
+    sed -i "s/@R_LABEL@/$RL/gi" $SRC_FILE
+    sed -i "s/@G_LABEL@/$GL/gi" $SRC_FILE
+    sed -i "s/@B_LABEL@/$BL/gi" $SRC_FILE
 
     rm -f sass/air-dark.css # clean up CSS
 else
